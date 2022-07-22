cc38da5cb4ebdf2d33b89d89c070027dcb864bd7
Gus Cardozo <gustavo.cardozo0@gmail.com>
2022-07-22 00:09:41 -0300

    Primeros modelos creados

---
+++ JuguetesWooden.MVC/Models/Entities/CategoriaJuguete.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace JuguetesWooden.Models
+{
+    public class CategoriaJuguete
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Juguete> Juguetes { get; set; }
+    }
+}
---
+++ JuguetesWooden.MVC/Models/Entities/DetalleCompra.cs
@@ -0,0 +1,13 @@
+﻿namespace JuguetesWooden.Models
+{
+    public class DetalleCompra
+    {
+        public int Id { get; set; }
+        public int Cantidad { get; set; }
+        public int IdJuguete { get; set; }
+        public int IdCarritoCompra { get; set; }
+        public double Subtotal { get; set; }
+        public virtual Juguete Juguete { get; set; }
+        public virtual CarritoCompra CarritoCompra { get; set; }
+    }
+}
