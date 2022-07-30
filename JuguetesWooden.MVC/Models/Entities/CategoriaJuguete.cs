247dc5b41c1646fb9f621763427deb217d4a0788
Gus Cardozo <gustavo.cardozo0@gmail.com>
2022-07-30 17:18:49 -0300

    Map de modelos

--- JuguetesWooden.MVC/Models/Entities/CategoriaJuguete.cs
+++ JuguetesWooden.MVC/Models/Entities/CategoriaJuguete.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
 
 namespace JuguetesWooden.Models
 {
@@ -6,6 +8,19 @@ namespace JuguetesWooden.Models
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public List<Juguete> Juguetes { get; set; }
+        public virtual ICollection<Juguete> Juguetes { get; set; }
+    }
+
+    public class CategoriaJugueteMap : IEntityTypeConfiguration<CategoriaJuguete>
+    {
+        public void Configure(EntityTypeBuilder<CategoriaJuguete> builder)
+        {
+            builder.ToTable("CategoriasJuguetes");
+            builder.HasKey(x => x.Id);
+            builder.Property(s => s.Id).HasColumnName("id").ValueGeneratedOnAdd().IsRequired();
+            builder.Property(s => s.Nombre).HasColumnName("Nombre").HasMaxLength(100);
+
+            builder.HasMany(x => x.Juguetes).WithOne();
+        }
     }
 }
--- JuguetesWooden.MVC/Models/Entities/DetalleCompra.cs
+++ JuguetesWooden.MVC/Models/Entities/DetalleCompra.cs
@@ -1,4 +1,7 @@
-﻿namespace JuguetesWooden.Models
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JuguetesWooden.Models
 {
     public class DetalleCompra
     {
@@ -6,8 +9,25 @@
         public int Cantidad { get; set; }
         public int IdJuguete { get; set; }
         public int IdCarritoCompra { get; set; }
-        public double Subtotal { get; set; }
+        public decimal Subtotal { get; set; }
         public virtual Juguete Juguete { get; set; }
         public virtual CarritoCompra CarritoCompra { get; set; }
+
+        public class DetalleCompraMap : IEntityTypeConfiguration<DetalleCompra>
+        {
+            public void Configure(EntityTypeBuilder<DetalleCompra> builder)
+            {
+                builder.ToTable("DetalleCompras");
+                builder.HasKey(x => x.Id);
+                builder.Property(s => s.Id).HasColumnName("Id").ValueGeneratedOnAdd().IsRequired();
+                builder.Property(s => s.Cantidad).HasColumnName("Cantidad").IsRequired();
+                builder.Property(s => s.Subtotal).HasColumnName("Subtotal").HasColumnType("decimal(18, 4)").IsRequired();
+                builder.Property(s => s.IdJuguete).HasColumnName("IdJuguete").IsRequired();
+                builder.Property(s => s.IdCarritoCompra).HasColumnName("IdCarritoCompra").IsRequired();
+
+                builder.HasOne(x => x.Juguete).WithMany().HasForeignKey(x => x.IdJuguete).IsRequired().HasConstraintName("FK_DetalleCompras_Juguetes");
+                builder.HasOne(x => x.CarritoCompra).WithMany().HasForeignKey(x => x.IdCarritoCompra).IsRequired().HasConstraintName("FK_DetalleCompras_CarritoCompras");
+            }
+        }
     }
 }
