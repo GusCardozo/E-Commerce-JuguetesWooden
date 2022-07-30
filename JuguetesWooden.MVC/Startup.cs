5293bc01d17074297224cce2e15a4deadc34e945
Gus Cardozo <gustavo.cardozo0@gmail.com>
2022-07-30 17:18:14 -0300

    Se crea Context y configuracion necesaria

--- JuguetesWooden.MVC/Startup.cs
+++ JuguetesWooden.MVC/Startup.cs
@@ -1,15 +1,12 @@
+using JuguetesWooden.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace JuguetesWooden.MVC
+namespace JuguetesWooden
 {
     public class Startup
     {
@@ -24,6 +21,10 @@ namespace JuguetesWooden.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddEntityFrameworkSqlServer();
+            services.AddDbContext<WoodenContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
